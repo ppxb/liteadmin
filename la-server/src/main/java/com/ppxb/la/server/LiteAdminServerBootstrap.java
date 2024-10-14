4267029f773abfe5a4e665bd3095bda6fc7594bb
ppxb <ppxb123@gmail.com>
2024-10-14 21:22:48 +0800

    chore: add aop dependency

--- la-server/src/main/java/com/ppxb/la/server/LiteAdminServerBootstrap.java
+++ la-server/src/main/java/com/ppxb/la/server/LiteAdminServerBootstrap.java
@@ -2,9 +2,17 @@ package com.ppxb.la.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableCaching
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@ComponentScan(LiteAdminServerBootstrap.COMPONENT_SCAN)
 @SpringBootApplication
 public class LiteAdminServerBootstrap {
+
+    public static final String COMPONENT_SCAN = "com.ppxb.la";
 
     public static void main(String[] args) {
         SpringApplication.run(LiteAdminServerBootstrap.class, args);
